8d9b0bbc0ccfb140ccbe2e7e2510b5d2a949ebcf
root <root@LAPTOP-L013TINN.localdomain>
2020-10-19 21:08:54 +0800

    new file

---
+++ bash_script.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+for((i=1;i<101;i++)); do
+	        mkdir DDM$i
+		        cd DDM$i
+			        echo "nanoseconds since 1970-01-01 00:00:00 UTC: " > time_till_now.txt
+				        date "+%s" >> time_till_now.txt
+					        cd ..
+					done
+					:
